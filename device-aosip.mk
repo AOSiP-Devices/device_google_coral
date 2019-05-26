eb10bcf6da3c76c3734850dd18d534d4ed4debf5
razorloves <razorloves@gmail.com>
2019-05-26 04:14:33 -0500

    coral: Add missing vendor packages and properties
    Change-Id: I88bdf8b2522b751c9490820b0ef55ee53dd35d1d

--- device-aosip.mk
+++ device-aosip.mk
@@ -23,6 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosip
 AB_OTA_PARTITIONS += \
     vendor
 
+# Properties
+TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
+
 # Vendor packages
 PRODUCT_PACKAGES += \
     ese_spi_st \
@@ -31,7 +34,9 @@ PRODUCT_PACKAGES += \
     libcld80211 \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
+    libdisplayconfig \
     libdrm.vendor \
+    libjson \
     libmedia_ecoservice.vendor \
     libnetfilter_conntrack \
     libnfnetlink \
@@ -46,6 +51,8 @@ PRODUCT_PACKAGES += \
     libstagefright_bufferpool@2.0.1.vendor \
     libtextclassifier_hash.vendor \
     libtinycompress \
+    libtinyxml \
+    libwifi-hal-qcom \
     nos_app_avb \
     nos_app_keymaster \
     nos_app_weaver \
