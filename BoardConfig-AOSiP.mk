da51e2678c97c252194661c8220adf0894b04646
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    coral: Build a disabled vbmeta image
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfig-AOSiP.mk
+++ BoardConfig-AOSiP.mk
@@ -24,6 +24,10 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/floral
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 -include vendor/google/coral/BoardConfigVendor.mk
 
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
