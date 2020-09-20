72b693ef0aa105210de735df25ddfa694aa0ff21
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-09-20 10:55:29 -0400

    coral: build with gapps
    Change-Id: I1c6c3b4d888b8d32da52d5d55c378c42fb82a49d

--- aosip_coral.mk
+++ aosip_coral.mk
@@ -43,4 +43,5 @@ BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
 
+$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 $(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
